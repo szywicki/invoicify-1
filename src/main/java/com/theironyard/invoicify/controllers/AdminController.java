e2ca3cdc008519a53fc0f623822a75447be08aab
Serena Zywicki <serena.zywicki@libertymutual.com>
2017-09-15 14:08:41 -0500

    post for adding company working

--- src/main/java/com/theironyard/invoicify/controllers/AdminController.java
+++ src/main/java/com/theironyard/invoicify/controllers/AdminController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -24,5 +25,11 @@ public class AdminController {
 		ModelAndView mv = new ModelAndView("admin/companies");
 		mv.addObject("companies", companyRepository.findAll());
 		return mv;
+	}
+	
+	@PostMapping("/companies")
+	public String createCompany(Company company) {
+		companyRepository.save(company);
+		return ("redirect:/admin/companies");
 	}
 }
